189729ef91463cc8e7f1671b424be37b5def542f
David Tsui <st880221@gmail.com>
2016-10-29 15:37:36 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+	<div id="fullpage">
+		<!-- <img class="logo" src="./assets/logo.png"> -->
+		<First></First>
+		<Second></Second>
+		<Third></Third>
+	</div>
+</template>
+
+<script>
+import First from './components/First'
+import Second from './components/Second'
+import Third from './components/Third'
+
+export default {
+	components: {
+		First,
+		Second,
+		Third
+	}
+}
+</script>
+
+<style lang="scss">
+html {
+	height: 100%;
+}
+
+body {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
+}
+
+#fullpage {
+	color: #2c3e50;
+	margin-top: -100px;
+	max-width: 600px;
+	font-family: Source Sans Pro, Helvetica, sans-serif;
+	text-align: center;
+	a {
+		color: #42b983;
+		text-decoration: none;
+	}
+}
+
+.logo {
+	width: 100px;
+	height: 100px
+}
+</style>
---
+++ src/components/Third.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="section" id="page3">
+    <button id="switch" v-on:click="show = !show">I am a switch~</button>
+    <transition name="bounce">
+      <p v-show="show">{{ msg }}</p>
+    </transition>
+    <p>小樓昨夜又東風2</p>
+    <p>故國不堪回首月明中2</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Page3!',
+      show: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+p {
+  font-size: 24px;
+  color: #42b983;
+}
+
+.fade-enter-active, .fade-leave-active {
+  animation: fly 1.6s forwards;
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0;
+}
+
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-out .5s;
+}
+
+@keyframes fly {
+  0% {
+    opacity: 1;
+    transform: translateX(0) translateY(0);
+  }
+  25% {
+    transform: translateX(20px) translateY(15px);
+  }
+  50% {
+    opacity: .65;
+    transform: translateX(120px) translateY(0);
+  }
+  75% {
+    transform: translateX(160px) translateY(-40px);
+  }
+  100% {
+    opacity: 0;
+    transform: translateX(200px) translateY(-30px);
+  }
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes bounce-out {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+
+</style>
